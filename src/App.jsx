00107beedb8cfa7693c995de67786f65a2f43584
jaalazard <ja.alazard@proton.me>
2023-09-28 17:21:10 +0200

    need modif checking

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,36 @@ function App() {
   let wordToGuess = "zoro";
 
   const [blanks, setBlanks] = useState("_".repeat(wordToGuess.length));
-  const [count, setCount] = useState(11);
+  const [count, setCount] = useState(10);
   const [userLetter, setUserLetter] = useState();
 
   const checking = (wordToGuess, userLetter) => {
-    if (wordToGuess.includes(userLetter)) {
       for (let i = 0; i < wordToGuess.length; i++) {
         if (wordToGuess[i] === userLetter) {
           setBlanks(blanks.replace(blanks[i], userLetter));
-        }
       }
     }
+    setCount(count-1);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const userInput = document.querySelector("#userInput");
     setUserLetter(userInput.value);
+    if (wordToGuess.includes(userLetter) && count > 0) {
     checking(wordToGuess, userLetter);
+    }
     if (blanks === wordToGuess) {
       alert("Gagné!");
-    }
-    setCount(count - 1);
-    if (count <= 0) {
-      alert("Perdu !");
     }
     userInput.value="";
   };
 
   return (
     <>
+    {count > 0 ?
+    <p>Essais restants : {count}</p>
+    : <p>Perdu !</p>}
       <form onSubmit={handleSubmit}>
         <label htmlFor="userInput">Entrez une lettre : </label>
         <input id="userInput"></input>
