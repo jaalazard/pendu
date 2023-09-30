64a178730b2f06c8f96d744c17938f8d0df6887b
jaalazard <ja.alazard@proton.me>
2023-09-30 13:19:23 +0200

    working better

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,69 @@ import "./App.css";
 function App() {
   let wordToGuess = "zoro";
 
-  const [blanks, setBlanks] = useState("_".repeat(wordToGuess.length));
+  const blanks = "_".repeat(wordToGuess.length);
   const [count, setCount] = useState(10);
   const [userLetter, setUserLetter] = useState();
+  const [guessedWord, setGuessedWord] = useState(blanks);
+  const [badLetters, setBadLetters] = useState([]);
 
   const checking = (wordToGuess, userLetter) => {
+    if (wordToGuess.includes(userLetter)) {
       for (let i = 0; i < wordToGuess.length; i++) {
         if (wordToGuess[i] === userLetter) {
-          setBlanks(blanks.replace(blanks[i], userLetter));
-      }
+         setGuessedWord((prev) => {
+          let newWord = prev.split('');
+          newWord[i] = userLetter;
+          return newWord.join('');
+         }); 
+      } 
+    } 
+  } else {
+        badLetters.push(userLetter);      
     }
-    setCount(count-1);
-  };
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const userInput = e.target.value;
+    setUserLetter(userInput[0] ?? "");
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const userInput = document.querySelector("#userInput");
-    setUserLetter(userInput.value);
-    if (wordToGuess.includes(userLetter) && count > 0) {
+    setCount(count-1);
     checking(wordToGuess, userLetter);
-    }
-    if (blanks === wordToGuess) {
+    if (guessedWord === wordToGuess) {
       alert("Gagné!");
-    }
-    userInput.value="";
+    }const input = document.querySelector('input');
+    input.value='';
   };
 
   return (
-    <>
+    <div id="container">
+      <div id="tries">
     {count > 0 ?
     <p>Essais restants : {count}</p>
     : <p>Perdu !</p>}
+    </div>
+    <div id="form">
       <form onSubmit={handleSubmit}>
         <label htmlFor="userInput">Entrez une lettre : </label>
-        <input id="userInput"></input>
+        <input id="userInput" onChange={handleChange}></input>
         <button>Valider</button>
       </form>
-      <p>{blanks}</p>
-    </>
+      </div>
+      <div id="guessedWordDisplay">
+      {guessedWord.split('').map((element, index) => (
+      <span className="span" key={index}>{element}</span>))}
+      </div>
+      <div id="badLetters">
+      <p>Tentatives précédentes : {badLetters.join(', ')}</p>
+      </div>
+    </div>
   );
 }
+
+
 
 export default App;
